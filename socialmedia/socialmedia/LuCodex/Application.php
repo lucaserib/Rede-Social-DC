f3862f8a6b0b95de29576f2a39be079def080e44
lucaserib <137096507+lucaserib@users.noreply.github.com>
2023-11-07 14:58:07 -0300

    Add files via upload

---
+++ socialmedia/socialmedia/LuCodex/Application.php
@@ -0,0 +1,41 @@
+<?php
+	namespace LuCodex;
+	class Application
+	{
+		private $controller;
+
+		private function setApp(){
+
+			$loadName = 'LuCodex\Controllers\\';
+			$url = explode('/',@$_GET['url']);
+
+			if($url[0] == ''){
+				$loadName.='Home';
+
+			}else{
+				$loadName.=ucfirst(strtolower($url[0]));
+			}
+
+			$loadName.='Controller';
+
+			if(file_exists($loadName.'.php')){
+				$this->controller = new $loadName();
+			}else{
+				include('Views/pages/404.php');
+				die();
+			}
+
+			
+			
+
+
+		}
+		
+		public function run(){
+			$this->setApp();
+			$this->controller->index();
+		}
+		
+	}
+
+?>
